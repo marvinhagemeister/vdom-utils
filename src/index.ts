471b86c62f64dc62d398b443c6397b57b3074498
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-06-23 09:46:26 +0200

    Add DOM element types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-export const VOID_ELEMENTS = new Set([
-  "area",
-  "base",
-  "br",
-  "col",
-  "embed",
-  "hr",
-  "img",
-  "input",
-  "link",
-  "meta",
-  "param",
-  "source",
-  "track",
-  "wbr",
-]);
+export * from "./dom";
 
 const ESC: Record<string, string> = {
   "<": "&lt;",
